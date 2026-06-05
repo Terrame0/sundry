7262590d91c280d8b97c0c7ab1e1e3e4c4275817
Terrame0 <terrame_0@proton.me>
2026-06-05 13:49:15 +1000

    fixed directory reformation

--- src/vfs/dir/reform.nix
+++ src/vfs/dir/reform.nix
@@ -1,3 +1,31 @@
-{mlem,...}:{
-  reform = mlem.attrs.reform-until (mlem.vfs.is-leaf);
+{
+  mlem,
+  flake-root,
+  ...
+}: rec {
+  reform = mlem.attrs.reform-until (path: mlem.vfs.is-leaf);
+  tests = [
+    [
+      (reform (path: file: {
+        inherit path;
+        value = file // {path = mlem.vfs.path.get.str path;};
+      }) (mlem.vfs.dir.from-real "${flake-root}/tests/vfs-test-dir/test-files"))
+      {
+        "a.txt" = {
+          contents = "contents of a.txt";
+          path = "a.txt";
+        };
+        "b.txt" = {
+          contents = "contents of b.txt";
+          path = "b.txt";
+        };
+        nested = {
+          "c.txt" = {
+            contents = "contents of c.txt";
+            path = "nested/c.txt";
+          };
+        };
+      }
+    ]
+  ];
 }
