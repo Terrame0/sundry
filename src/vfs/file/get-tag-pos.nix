d5752d636554c6221017014f53e70dd1be4fb0e2
Terrame0 <terrame_0@proton.me>
2026-06-21 00:56:53 +1000

    renamed 'get.tag-pos' to 'get-tag-pos'

--- src/vfs/file/get-tag-pos.nix
+++ src/vfs/file/get-tag-pos.nix
@@ -3,7 +3,7 @@
   lib,
   ...
 }: rec {
-  get.tag-pos = tag-spec: file:
+  get-tag-pos = tag-spec: file:
     lib.findFirst
     (i:
       sundry.vfs.tags-match
@@ -20,18 +20,18 @@
       tag-list = [];
     };
   in [
-    [(get.tag-pos {x = "1";} file) 0]
-    [(get.tag-pos {x = "2";} file) (-1)]
-    [(get.tag-pos {x = [];} file) 0]
-    [(get.tag-pos {x = ["1" "2"];} file) 0]
-    [(get.tag-pos ({y = "1";} // {z = "1";}) file) 1]
-    [(get.tag-pos ({y = "2";} // {z = "1";}) file) (-1)]
-    [(get.tag-pos ({y = [];} // {z = "1";}) file) 1]
-    [(get.tag-pos ({y = ["1" "2"];} // {z = "1";}) file) 1]
-    [(get.tag-pos ({x = "1";} // {y = "1";}) file) (-1)]
-    [(get.tag-pos {x = "1";} blank) (-1)]
-    [(get.tag-pos {x = null;} file) 1]
-    [(get.tag-pos {w = null;} file) 0]
-    [(get.tag-pos ({y = null;} // {z = "1";}) file) (-1)]
+    [(get-tag-pos {x = "1";} file) 0]
+    [(get-tag-pos {x = "2";} file) (-1)]
+    [(get-tag-pos {x = [];} file) 0]
+    [(get-tag-pos {x = ["1" "2"];} file) 0]
+    [(get-tag-pos ({y = "1";} // {z = "1";}) file) 1]
+    [(get-tag-pos ({y = "2";} // {z = "1";}) file) (-1)]
+    [(get-tag-pos ({y = [];} // {z = "1";}) file) 1]
+    [(get-tag-pos ({y = ["1" "2"];} // {z = "1";}) file) 1]
+    [(get-tag-pos ({x = "1";} // {y = "1";}) file) (-1)]
+    [(get-tag-pos {x = "1";} blank) (-1)]
+    [(get-tag-pos {x = null;} file) 1]
+    [(get-tag-pos {w = null;} file) 0]
+    [(get-tag-pos ({y = null;} // {z = "1";}) file) (-1)]
   ];
 }
