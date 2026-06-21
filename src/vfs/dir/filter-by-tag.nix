dd3dd8916bdcbcb16342380c24df01d600202e91
Terrame0 <terrame_0@proton.me>
2026-06-21 11:59:54 +1000

    'filter-by-tag' now can recieve a disjunctive list of specs

--- src/vfs/dir/filter-by-tag.nix
+++ src/vfs/dir/filter-by-tag.nix
@@ -5,8 +5,10 @@
   ...
 }: {
   filter-by-tag = tag:
-    sundry.vfs.dir.filter (path: file:
-      sundry.vfs.tags-match tag (sundry.attrs.merge.concat file.tag-list));
+    sundry.vfs.dir.filter (path: file: let
+      tags = sundry.attrs.merge.concat file.tag-list;
+    in
+      lib.any (spec: sundry.vfs.tags-match spec tags) (lib.toList tag));
 
   tests = let
     dir = lib.pipe "${flake-root}/tests/vfs-test-dir/tags" [
@@ -21,6 +23,10 @@
     [
       (sundry.vfs.dir.collapse (path: file: file.text) (sundry.vfs.dir.filter-by-tag {b = null;} dir))
       ["contents of F" "contents of A" "contents of B" "contents of E"]
+    ]
+    [
+      (sundry.vfs.dir.collapse (path: file: file.text) (sundry.vfs.dir.filter-by-tag [{b = "1";} {b = null;}] dir))
+      ["contents of H" "contents of G" "contents of F" "contents of I" "contents of A" "contents of B" "contents of C" "contents of E"]
     ]
   ];
 }
