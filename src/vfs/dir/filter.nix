7177410d7b7f575272fe8329a7cb111b6dc8f7d4
Terrame0 <terrame_0@proton.me>
2026-06-30 18:45:26 +1000

    added 'select-by-tag' as a way to get rid of unmatched files, instead of selecting a subset to operate on and leaving unmatched files as-is

--- src/vfs/dir/filter.nix
+++ src/vfs/dir/filter.nix
@@ -13,6 +13,10 @@
     (path: file: sundry.boolean.expr expr-fn (sundry.attrs.merge.concat file.tag-list))
     sundry.vfs.is-leaf-node;
 
+  select-by-tag = expr-fn:
+    sundry.vfs.dir.filter
+    (path: file: sundry.boolean.expr expr-fn (sundry.attrs.merge.concat file.tag-list));
+
   tests = let
     filter-dir = sundry.vfs.dir.from-src "${flake-root}/tests/vfs-test-dir/filtering";
     tag-dir = lib.pipe "${flake-root}/tests/vfs-test-dir/tags" [
@@ -81,6 +85,39 @@
         "contents of I"
         "contents of B"
         "contents of D"
+        "contents of E"
+      ]
+    ]
+    [
+      (sundry.vfs.dir.collapse
+        (path: file: file.text)
+        (sundry.vfs.dir.select-by-tag
+          (e: with e; tag {b = "1";})
+          tag-dir))
+      ["contents of H" "contents of G" "contents of I" "contents of C"]
+    ]
+    [
+      (sundry.vfs.dir.collapse
+        (path: file: file.text)
+        (sundry.vfs.dir.select-by-tag
+          (e: with e; !(tag {b = [];}))
+          tag-dir))
+      ["contents of F" "contents of A" "contents of B" "contents of E"]
+    ]
+    [
+      (sundry.vfs.dir.collapse
+        (path: file: file.text)
+        (sundry.vfs.dir.select-by-tag
+          (e: with e; (tag {b = "1";}) || !(tag {b = [];}))
+          tag-dir))
+      [
+        "contents of H"
+        "contents of G"
+        "contents of F"
+        "contents of I"
+        "contents of A"
+        "contents of B"
+        "contents of C"
         "contents of E"
       ]
     ]
